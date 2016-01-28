7ec48b79224bfc8bc3161b560362f32437136bf6
kassoss <kassoran@gmail.com>
2016-01-27 19:57:44 -0500

    Getting articles.json with Ajax

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>.Mic Test - Ron Kassovitz</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -18,12 +18,21 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
+    
+    <div class="container">
+	    
+	    <div id="results">
+	    </div>
+    
+    
+    </div> <!-- END CONTAINER -->
+    
+    
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <script src="main.js"></script>
   </body>
 </html>
 
